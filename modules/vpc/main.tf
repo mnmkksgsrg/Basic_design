94a4543da00edff4a32d2c3b9a682fad2ecdb6e0
mnmkksgsrg <2002syunsuke@gmail.com>
2025-07-31 18:24:03 +0900

    add memo

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -69,33 +69,33 @@ resource "aws_security_group" "allow_web_traffic" {
 resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_ipv4" {
   security_group_id = aws_security_group.allow_web_traffic.id
   cidr_ipv4         = aws_vpc.this.cidr_block
-  from_port         = 443
+  from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 80
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_HTTP_ipv6" {
   security_group_id = aws_security_group.allow_web_traffic.id
   cidr_ipv6         = aws_vpc.this.ipv6_cidr_block
-  from_port         = 443
+  from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 80
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_HTTPS_ipv4" {
   security_group_id = aws_security_group.allow_web_traffic.id
   cidr_ipv4         = aws_vpc.this.cidr_block
-  from_port         = 80
+  from_port         = 443
   ip_protocol       = "tcp"
-  to_port           = 80
+  to_port           = 443
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_HTTPS_ipv6" {
   security_group_id = aws_security_group.allow_web_traffic.id
   cidr_ipv6         = aws_vpc.this.ipv6_cidr_block
-  from_port         = 80
+  from_port         = 443
   ip_protocol       = "tcp"
-  to_port           = 80
+  to_port           = 443
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
