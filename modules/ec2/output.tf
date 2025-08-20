ad60c4935f3bf6ca9ac92612430869e55fb00a1d
mnmkksgsrg <2002syunsuke@gmail.com>
2025-08-20 09:59:00 +0900

    fix

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,26 +1,26 @@
 output "instance_id" {
   description = "EC2インスタンスID"
-  value       = aws_instance.wordpress.id
+  value       = aws_instance.basic.id
 }
 
 output "instance_arn" {
   description = "EC2インスタンスARN"
-  value       = aws_instance.wordpress.arn
+  value       = aws_instance.basic.arn
 }
 
 output "private_ip" {
   description = "プライベートIPアドレス"
-  value       = aws_instance.wordpress.private_ip
+  value       = aws_instance.basic.private_ip
 }
 
 output "public_ip" {
   description = "パブリックIPアドレス"
-  value       = aws_instance.wordpress.public_ip
+  value       = aws_instance.basic.public_ip
 }
 
 output "availability_zone" {
   description = "アベイラビリティゾーン"
-  value       = aws_instance.wordpress.availability_zone
+  value       = aws_instance.basic.availability_zone
 }
 
 output "ami_id" {
