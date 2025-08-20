2a6c8c0669e70b78696d518d7c6411ba07249900
mnmkksgsrg <2002syunsuke@gmail.com>
2025-08-20 18:23:54 +0900

    fix

---
+++ modules/security_group/main.tf
@@ -0,0 +1,59 @@
+resource "aws_security_group" "web" {
+  name        = "${var.vpc_name}_web_security_group"
+  description = "webサーバーのセキュリティグループ"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "${var.vpc_name}_web_security_group"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.web.id
+  from_port         = 80
+  to_port           = 80
+  ip_protocol          = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.web.id
+  from_port         = 443
+  to_port           = 443
+  ip_protocol          = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_web" {
+  security_group_id = aws_security_group.web.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol          = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_security_group" "rds" {
+  name        = "${var.name}_rds_security_group"
+  description = "rdsのセキュリティグループ"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "${var.vpc_name}_rds_security_group"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "app_to_rds" {
+  security_group_id            = aws_security_group.rds.id
+  referenced_security_group_id = aws_security_group.web.id
+  from_port                    = 3306
+  to_port                      = 3306
+  ip_protocol                  = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_rds" {
+  security_group_id            = aws_security_group.rds.id
+  from_port                    = 0
+  to_port                      = 0
+  ip_protocol                  = "-1"
+  cidr_ipv4                    = "0.0.0.0/0"
+}
